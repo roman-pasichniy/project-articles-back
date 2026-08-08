1a8565ac4475d8ca8beff4b87eb5b3d03b05c75e
FinJex <maks.vasylovych12@gmail.com>
2026-08-08 23:18:13 +0200

    add login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-
+import { celebrate } from 'celebrate';
 import { auth as ctrl } from "../controllers/index.js";
-
+import { loginUser } from '../controllers/auth/authController.js';
+import { loginUserSchema} from '../validations/userValidation.js';
 export const authRouter = Router();
+
+router.post('/login', celebrate(loginUserSchema), loginUser);
 
 authRouter.post("/register", ctrl.registerUser);
