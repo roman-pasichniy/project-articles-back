f8a2a4635bc64f2c69b59eaf94fe129f4a14f015
Natalia Filtanovych <natashe4ka21092016@gmail.com>
2026-08-08 19:01:05 +0300

    fix validation schema

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,5 +1,11 @@
 import { Joi, Segments } from "celebrate";
 
+export const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value)
+    ? helpers.message("Невірний формат ідентифікатора")
+    : value;
+};
+
 export const userIdParamSchema = {
   [Segments.PARAMS]: Joi.object({
     userId: Joi.string().custom(objectIdValidator).required(),
