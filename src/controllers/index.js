d261f07b6e05e9039f96a362e22f8e8fceb78065
Sergio Shambir <realistic.sergio@gmail.com>
2026-08-08 20:56:46 +0200

    feat: add article filtering sorting and categories

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,16 @@
 import { getArticleById } from "./articles/getArticleById.js";
 import { registerUser } from "./auth/registerUser.js";
 import { getUserById } from "./users/getUserById.js";
+import { getArticles } from "./articles/getArticles.js";
+import { getArticleCategories } from "./articles/getArticleCategories.js";
 
 export const auth = {
   registerUser,
 };
 export const articles = {
+  getArticles,
   getArticleById,
+  getArticleCategories,
 };
 
 export const users = {
