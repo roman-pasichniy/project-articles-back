44436f985460f35fdcee933756232028bb5fcbd8
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-08-09 11:58:39 +0300

    add auth/currentUser

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,17 +1,20 @@
-const jwt = require("jsonwebtoken");
-const { auth } = require("../controllers");
+import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith) "Bearer";
-    return res.status(401).json("No token or not wright format");
+    if (!authHeader || !authHeader.startsWith("Bearer "))
+      return res.status(401).json({ message: "No token or not wright format" });
 
-    const token = authHeader.split("")[1];
+    const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+
+    next();
   } catch (error) {
     return res.status(401).json({ message: "Wrong token" });
   }
 };
+
+export default authMiddleware;
