23ce4c7380d406641ba8424f732034d922936499
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-08-09 14:26:28 +0300

    fix: authRoutes: authMiddleware, ctrl.currentUser

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
-import { celebrate } from 'celebrate';
+import { celebrate } from "celebrate";
 import { auth as ctrl } from "../controllers/index.js";
-import { loginUser } from '../controllers/auth/authController.js';
-import { loginUserSchema} from '../validations/userValidation.js';
+import { loginUser } from "../controllers/auth/authController.js";
+import { loginUserSchema } from "../validations/userValidation.js";
 export const authRouter = Router();
 
-router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post("/login", celebrate(loginUserSchema), loginUser);
 
 authRouter.post("/register", ctrl.registerUser);
-authRouter.get("/current", ctrl.currentUser, authMiddleware);
+authRouter.get("/current", authMiddleware, ctrl.currentUser);
