104374bbc94b01d0e9cd66b2196b4ad3f9840fb1
Nataliia Demchenko <natadem2109@gmail.com>
2026-08-09 10:38:29 +0300

    Add Cloudinary image upload

--- src/controllers/articles/createArticle.js
+++ src/controllers/articles/createArticle.js
@@ -1,15 +1,20 @@
 import createHttpError from "http-errors";
 
 import { createArticle } from "../../services/articles.js";
+import { saveFileToCloudinary } from "../../utils/saveFileToCloudinary.js";
 
 export const createArticleController = async (req, res) => {
   if (!req.file) {
     throw createHttpError(400, "Photo is required");
   }
 
+  const uploadResult = await saveFileToCloudinary(req.file.buffer, {
+    folder: "articles",
+  });
+
   const article = await createArticle({
     ...req.body,
-    photo: req.file.originalname,
+    photo: uploadResult.secure_url,
   });
 
   res.status(201).json({
