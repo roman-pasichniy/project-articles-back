309951f69c8b1a5c6118c568b6a9b2f284a76414
Petrovskyi <ivan.prog74@gmail.com>
2026-08-10 19:04:30 +0200

    fix

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { users as ctrl } from "../controllers/index.js";
 import { userIdParamSchema } from "../validations/userValidation.js";
 import { celebrate } from "celebrate";
-
 export const usersRouter = Router();
 usersRouter.get(
   "/users/:userId",
