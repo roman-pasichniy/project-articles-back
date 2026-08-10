f3337831a8d28ea8022abba22c102faaf9000d07
Petrovskyi <ivan.prog74@gmail.com>
2026-08-10 19:10:31 +0200

    fix auth middleware and restore users routes

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,11 +2,26 @@ import { Router } from "express";
 import { users as ctrl } from "../controllers/index.js";
 import { userIdParamSchema } from "../validations/userValidation.js";
 import { celebrate } from "celebrate";
+import authMiddleware from "../middleware/authMiddleware.js";
+
 export const usersRouter = Router();
+
 usersRouter.get(
-  "/users/:userId",
+  "/me/saved-articles",
+  authMiddleware,
+  ctrl.getSavedArticles,
+);
+
+usersRouter.get(
+  "/:userId",
   celebrate(userIdParamSchema),
   ctrl.getUserById,
+);
+
+usersRouter.get(
+  "/:userId/articles",
+  celebrate(userIdParamSchema),
+  ctrl.getUserArticles,
 );
 
 usersRouter.get("/", ctrl.getAuthors);
