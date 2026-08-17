e952accdd2a17d8e7fe166d1308acd0490725a93
Serhiy Ponomarenko <ponomarenko404@gmail.com>
2026-08-17 22:31:51 +0300

    fix: update saved articles

--- src/controllers/users/getSavedArticles.js
+++ src/controllers/users/getSavedArticles.js
@@ -1,25 +1,32 @@
-import createHttpError from "http-errors";
 import { ArticleModel } from "../../models/article.js";
+import createHttpError from "http-errors";
 
 export const getSavedArticles = async (req, res) => {
   const user = req.user;
 
- const page = parseInt(req.query.page) || 1;
-const perPage = parseInt(req.query.perPage) || 10;
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 12;
   const skip = (page - 1) * perPage;
 
   const totalItems = user.savedArticles.length;
 
   const articleIds = user.savedArticles.slice(skip, skip + perPage);
 
-  const articles = await ArticleModel.find({
-    _id: { $in: articleIds },
-  }).select("photo title description date author ownerId");
+ const articles = await ArticleModel.find({
+  _id: { $in: articleIds },
+}).select("img title desc date ownerId");
 
   const totalPages = Math.ceil(totalItems / perPage);
 
   res.status(200).json({
-    articles,
+    articles: articles.map(article => ({
+  _id: article._id,
+  img: article.img,
+  title: article.title,
+  desc: article.desc,
+  date: article.date,
+  ownerId: article.ownerId,
+})),
     pagination: {
       page,
       perPage,
